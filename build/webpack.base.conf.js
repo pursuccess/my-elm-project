5c5f9f2359a877dec7fcc4eda239ebe647421b89
pursuccess <976482580@qq.com>
2017-08-14 18:01:29 +0800

    添加stylus支持及文件

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,8 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src')
+      '@': resolve('src'),
+      // 'fonts': resolve('scr/common/fonts')
     }
   },
   module: {
